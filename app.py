a086882cd72f3eb1ce9532ce8083fcd92ce8e78e
AJCortex <anthonycortez3d@gmail.com>
2026-04-15 14:31:38 -0700

    Fix imports and requirements

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import os
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_pinecone import PineconeVectorStore
-from langchain.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 
